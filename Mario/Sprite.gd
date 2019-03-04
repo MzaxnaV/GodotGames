b4e300573930218d9fe7ccb00ef0d8997da00a71
MzaxnaV <manav.zxa@gmail.com>
2019-03-04 19:22:18 +0530

    character4

--- Mario/Sprite.gd
+++ Mario/Sprite.gd
@@ -1,15 +1,43 @@
 extends Sprite
 
-var CAMERA_SCROLL_SPEED = 40
+# SHOULD BE USING STATE MACHINE FOR THIS
+
+const PLAYER_SPEED = 40
+const GRAVITY = 7
+const JUMP_VLOCITY = -200
+
+var velocity = 0
+
+var jumping = false
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	
+	if event.is_action_pressed("ui_up") and velocity == 0:
+		velocity = JUMP_VLOCITY
+		$AnimationPlayer.current_animation = "jump"
+		jumping = true
 
 func _process(delta):
+	velocity += GRAVITY
+	position.y += velocity * delta
+	
 	if Input.is_action_pressed("ui_left"):
 		flip_h = true
-		position.x -= CAMERA_SCROLL_SPEED * delta
-		$AnimationPlayer.current_animation = "walk"
-	elif Input.is_action_pressed("ui_right"):
+		position.x -= PLAYER_SPEED * delta
+		if !jumping:
+			$AnimationPlayer.current_animation = "walk"
+	elif  Input.is_action_pressed("ui_right"):
 		flip_h = false
-		position.x += CAMERA_SCROLL_SPEED * delta
-		$AnimationPlayer.current_animation = "walk"
-	else:
+		position.x += PLAYER_SPEED * delta
+		if !jumping:
+			$AnimationPlayer.current_animation = "walk"
+	elif !jumping:
 		$AnimationPlayer.current_animation = "idle"
+
+	if position.y > 70:
+		position.y = 70
+		velocity = 0
+		jumping = false
+

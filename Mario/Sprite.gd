8d42eec9254e1df757808ec95c4070ed6c68535b
MzaxnaV <manav.zxa@gmail.com>
2019-03-04 18:25:23 +0530

    character2-4

--- Mario/Sprite.gd
+++ Mario/Sprite.gd
@@ -4,6 +4,12 @@ var CAMERA_SCROLL_SPEED = 40
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
+		flip_h = true
 		position.x -= CAMERA_SCROLL_SPEED * delta
+		$AnimationPlayer.current_animation = "walk"
 	elif Input.is_action_pressed("ui_right"):
+		flip_h = false
 		position.x += CAMERA_SCROLL_SPEED * delta
+		$AnimationPlayer.current_animation = "walk"
+	else:
+		$AnimationPlayer.current_animation = "idle"

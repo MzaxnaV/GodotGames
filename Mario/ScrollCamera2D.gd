3d87b9d67dd0d66f343cbd0841ba6e450944c304
MzaxnaV <manav.zxa@gmail.com>
2019-03-04 17:14:58 +0530

    character1

--- Mario/ScrollCamera2D.gd
+++ Mario/ScrollCamera2D.gd
@@ -1,9 +1,2 @@
 extends Camera2D
 
-var CAMERA_SCROLL_SPEED = 40
-
-func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		position.x -= CAMERA_SCROLL_SPEED * delta
-	elif Input.is_action_pressed("ui_right"):
-		position.x += CAMERA_SCROLL_SPEED * delta
